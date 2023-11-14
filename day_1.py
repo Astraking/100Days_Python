459d18dfbf70192502947b437bb69757cd2ccc89
Astraking <kofiadjeiboateng5@gmail.com>
2023-11-14 17:10:58 +0000

    Day 1 Python file

---
+++ day_1.py
@@ -0,0 +1,10 @@
+# Band Name Generator
+name = input("What is your name?\n")
+print("Hi there", name)
+print("\nWelcome to the Band Name Generator!")
+city = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = city + " " + pet_name
+print("Your band name is: ", band_name)
+print("Bye,", name)
+
